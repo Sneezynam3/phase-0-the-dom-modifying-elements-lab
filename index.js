27191b371f50b13fd9242260236e1321e9913001
James Snell <vulpes3.1415@yahoo.com>
2023-05-24 08:49:11 -0600

    phase-0-the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+
+//remove the DOM node main
+main.remove();
+
+//has a 'newHeader' variable that points to an <h1> node
+//the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+//<h1> with id 'victory' includes the text "is the champion"
+const newHeader = document.createElement("h1");
+newHeader.id="victory"
+newHeader.textContent="is the champion"
+document.body.append(newHeader);
+element.append(newHeader);
